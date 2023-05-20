48bde494acf7e0b4158a7a2283409136ce09597f
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2023-05-20 13:25:32 +0000

    Created forms.routes.vote.Create.Template; Edited forms.routes.vote.Create

--- client_code/forms/routes/vote/Create/__init__.py
+++ client_code/forms/routes/vote/Create/__init__.py
@@ -2,6 +2,9 @@ from ._anvil_designer import CreateTemplate
 import anvil
 from anvil_extras import routing
 from app.model import Ballot
+from app import globals
+
+columns = [{"field": "name"}]
 
 
 @routing.route("vote/{ballot_id}")
@@ -9,9 +12,24 @@ class Create(CreateTemplate):
     def __init__(self, **properties):
         uuid = self.dynamic_vars["ballot_id"]
         self.ballot = Ballot.get(uuid)
-        self.init_components(**properties)
-
-    def form_show(self, **event_arg):
+        self.selection = ""
         if not self.ballot.uuid:
             anvil.alert("No valid ballot found with that id.")
             routing.route("")
+
+        self.init_components(**properties)
+        self.tabulator.data = [{"name": c} for c in self.ballot.candidates]
+        self.tabulator.options = {"index": "name", "selectable": "highlight"}
+        self.tabulator.columns = columns
+
+    def tabulator_row_click(self, row, **event_args):
+        self.selection = row.get_index()
+        self.refresh_data_bindings()
+
+    def vote_button_click(self, **event_args):
+        anvil.alert(f"Not implemented yet. ({self.selection})")
+
+    def cancel_button_click(self, **event_args):
+        routing.set_url_hash("")
+
+
