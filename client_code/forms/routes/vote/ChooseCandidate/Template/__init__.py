08860df7020bfea6284f3c55d77ac37121f1a782
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2023-05-21 10:24:15 +0000

    Remove anvil boilerplate

--- client_code/forms/routes/vote/ChooseCandidate/Template/__init__.py
+++ client_code/forms/routes/vote/ChooseCandidate/Template/__init__.py
@@ -1,9 +1,6 @@
 from ._anvil_designer import TemplateTemplate
-from anvil import *
+
 
 class Template(TemplateTemplate):
     def __init__(self, **properties):
-        # Set Form properties and Data Bindings.
         self.init_components(**properties)
-
-        # Any code you write here will run before the form opens.
