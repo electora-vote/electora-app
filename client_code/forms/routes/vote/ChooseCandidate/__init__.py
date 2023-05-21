50c7c5a8b0bc65f8b1478e8274e716a22ac74c77
James Campbell <james.campbell@tanti.org.uk>
2023-05-21 09:40:48 +0200

    Add functions for encrypting and decrypting

--- client_code/forms/routes/vote/ChooseCandidate/__init__.py
+++ client_code/forms/routes/vote/ChooseCandidate/__init__.py
@@ -4,6 +4,7 @@ from anvil.js import window
 from anvil_extras import routing
 from app.model import Ballot
 from app import globals
+from anvil.js.window import ethereum 
 
 columns = [{"field": "name"}]
 
@@ -27,6 +28,20 @@ class ChooseCandidate(ChooseCandidateTemplate):
     def vote_button_click(self, **event_args):
         kwargs = {"ballot_id": self.ballot.uuid, "selection": self.selection, "proof": self.proof}
         routing.set_url_hash(url_pattern="vote/cast", url_dict=kwargs)
+
+    def encrypt_vote(self, proof, vote):
+        encrypted_vote = anvil.server.call("encrypt_vote", proof, vote, self.ballot.ends_at.timestamp())
+        return encrypted_vote
+
+    def decrypt_vote(self, ciphertext):
+        decrypted_vote = anvil.server.call("decrypt_vote", ciphertext, self.ballot.ends_at.timestamp())
+        return decrypted_vote
+
+    def submit_encrypted_vote(self, ciphertext):
+        provider = globals.ethers.providers.Web3Provider(ethereum)
+        signer = provider.getSigner()
+        ballot_manager_contract = globals.ethers.Contract(globals.ballot_manager_address, globals.ballot_manager_abi, signer)
+        ballot_manager_contract.vote(self.ballot.uuid, ciphertext)
 
     def cancel_button_click(self, **event_args):
         routing.set_url_hash("")
