eed9e28159812d39b162f68365e463e33d0aa76c
James Campbell <james.campbell@tanti.org.uk>
2023-05-21 10:38:35 +0200

    Encrypt vote+proof and write to chain

--- client_code/forms/routes/vote/Cast/__init__.py
+++ client_code/forms/routes/vote/Cast/__init__.py
@@ -2,7 +2,8 @@ from ._anvil_designer import CastTemplate
 from anvil_extras import routing
 import anvil
 from app.model import Ballot
-from app import globals
+from app import globals, nucypher
+from anvil.js.window import ethereum 
 
 
 @routing.route("vote/cast", url_keys=["ballot_id", "selection", "proof"])
@@ -12,9 +13,16 @@ class Cast(CastTemplate):
         self.ballot = Ballot.get(key=self.url_dict["ballot_id"])
         self.selection = self.url_dict["selection"]
         self.proof = self.url_dict["proof"]
+        self.encrypted_submission = nucypher.encrypt_vote(self.proof, self.selection, self.ballot.ends_at.timestamp())
         self.init_components(**properties)
 
+    def submit_encrypted_vote(self):
+        provider = globals.ethers.providers.Web3Provider(ethereum)
+        signer = provider.getSigner()
+        ballot_manager_contract = globals.ethers.Contract(globals.ballot_manager_address, globals.ballot_manager_abi, signer)
+        ballot_manager_contract.vote(self.ballot.uuid, self.encrypted_submission)
+
     def cast_button_click(self, **event_args):
-        anvil.alert("Not implemented yet.")
+        self.submit_encrypted_vote()
 
 
