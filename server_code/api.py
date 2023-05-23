082300ccc5de1b52085764cba4b64af5e1bbf8a3
Owen Campbell <owen.campbell@tanti.org.uk>
2023-05-23 13:14:53 +0100

    Move uplink code to server

--- server_code/api.py
+++ server_code/api.py
@@ -1,6 +1,9 @@
-import anvil.server
-from anvil.tables import app_tables, query
 from uuid import uuid4
+
+import anvil.server
+from anvil.tables import app_tables
+
+from . import _nucypher
 
 
 @anvil.server.callable
@@ -16,3 +19,13 @@ def add_ballot(attrs):
 @anvil.server.callable
 def get_ballot(attrs):
     return app_tables.ballot.get(uuid=attrs["uuid"])
+
+
+@anvil.server.callable
+def encrypt_vote(proof, vote, timestamp):
+    return _nucypher.encrypt_vote(proof, vote, timestamp)
+
+
+@anvil.server.callable
+def decrypt_vote(ciphertext, timestamp):
+    return _nucypher.decrypt_vote(ciphertext, timestamp)
