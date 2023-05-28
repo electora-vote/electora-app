2894aaad734ff87beee679f1dbe8623f5e3e675a
Owen Campbell <owen.campbell@tanti.org.uk>
2023-05-28 14:48:06 +0100

    Refactor model and services

--- client_code/forms/routes/vote/Cast/__init__.py
+++ client_code/forms/routes/vote/Cast/__init__.py
@@ -1,8 +1,8 @@
 from anvil.js.window import ethereum
 from anvil_extras import routing
-from app import globals
-from app.model import Ballot
-from app.services import nucypher
+from app import session
+from app.model import Ballot, Vote
+from app.services import encryption
 
 from ._anvil_designer import CastTemplate
 
@@ -10,27 +10,19 @@ from ._anvil_designer import CastTemplate
 @routing.route("vote/cast", url_keys=["ballot_id", "selection", "proof"])
 class Cast(CastTemplate):
     def __init__(self, **properties):
-        self.item[
-            "contract"
-        ] = f"https://scrollexplorer.unifra.io/address/{globals.ballot_manager_address}"
-        self.ballot = Ballot.get(key=self.url_dict["ballot_id"])
-        self.selection = self.url_dict["selection"]
-        self.proof = self.url_dict["proof"]
-        self.encrypted_submission = nucypher.encrypt_vote(
-            self.proof, self.selection, self.ballot.ends_at.timestamp()
+        self.contract = session.ONCHAIN_STORE.url
+        self.vote = Vote(
+            ballot=session.LOCAL_STORE.get(Ballot, self.url_dict["ballot_id"]),
+            selection=self.url_dict["selection"],
+            proof=self.url_dict["proof"],
+        )
+        self.ciphertext = encryption.encrypt_vote(
+            self.vote.proof, self.vote.selection, self.vote.ballot.ends_at.timestamp()
         )
         self.init_components(**properties)
 
-    def submit_encrypted_vote(self):
-        provider = globals.ethers.providers.Web3Provider(ethereum)
-        signer = provider.getSigner()
-        ballot_manager_contract = globals.ethers.Contract(
-            globals.ballot_manager_address, globals.ballot_manager_abi, signer
-        )
-        ballot_manager_contract.vote(self.ballot.uuid, self.encrypted_submission)
-
     def cast_button_click(self, **event_args):
-        self.submit_encrypted_vote()
+        session.ONCHAIN_STORE.cast_vote(self.ballot, self.ciphertext)
 
     def cancel_button_click(self, **event_args):
         routing.set_url_hash("")
---
+++ client_code/services/storage.py
@@ -0,0 +1,126 @@
+import anvil.js
+from anvil_extras.storage import indexed_db
+from . import encryption
+
+_ethers = anvil.js.import_from("ethers")
+
+try:
+    from anvil.js.window import ethereum  # noqa unused_import
+
+    _ethereum_available = True
+except AttributeError:
+    _ethereum_available = False
+
+
+class LocalStore:
+    """A wrapper for the browser's indexed DB
+
+    Each entry has a tuple of class name and uuid as its key.
+    """
+
+    store = indexed_db.create_store("eu.electora.objects")
+
+    @classmethod
+    def all(cls, obj_cls):
+        items = (v for k, v in cls.store.items() if k.split(",")[0] == obj_cls.__name__)
+        return (obj_cls(**item) for item in items)
+
+    @classmethod
+    def get(cls, obj_cls, uuid):
+        item = cls.store.get(f"{obj_cls.__name__},{uuid}", None)
+        return obj_cls(**item) if item else None
+
+    def save(self, obj):
+        key = getattr(obj, "key")
+        self.store[(obj.__class__.__name__, getattr(obj, key))] = obj.__dict__
+
+
+class OnChainStore:
+    address = "0x7ee88EB2209039fAb7e61Be48fAefEaCA492ACAD"
+    url = f"https://scrollexplorer.unifra.io/address/{address}"
+    abi = [
+        {"inputs": [], "stateMutability": "nonpayable", "type": "constructor"},
+        {
+            "inputs": [{"internalType": "string", "name": "", "type": "string"}],
+            "name": "ballots",
+            "outputs": [
+                {"internalType": "string", "name": "sismoGroupId", "type": "string"}
+            ],
+            "stateMutability": "view",
+            "type": "function",
+        },
+        {
+            "inputs": [
+                {"internalType": "string", "name": "_ballotId", "type": "string"},
+                {"internalType": "string", "name": "_sismoGroupId", "type": "string"},
+                {"internalType": "string[]", "name": "_candidates", "type": "string[]"},
+            ],
+            "name": "createBallot",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function",
+        },
+        {
+            "inputs": [
+                {"internalType": "string", "name": "_ballotId", "type": "string"}
+            ],
+            "name": "getCandidates",
+            "outputs": [{"internalType": "string[]", "name": "", "type": "string[]"}],
+            "stateMutability": "view",
+            "type": "function",
+        },
+        {
+            "inputs": [
+                {"internalType": "string", "name": "_ballotId", "type": "string"}
+            ],
+            "name": "getSismoGroupID",
+            "outputs": [{"internalType": "string", "name": "", "type": "string"}],
+            "stateMutability": "view",
+            "type": "function",
+        },
+        {
+            "inputs": [
+                {"internalType": "string", "name": "_ballotId", "type": "string"},
+                {
+                    "internalType": "string",
+                    "name": "_encryptedProofAndVote",
+                    "type": "string",
+                },
+            ],
+            "name": "vote",
+            "outputs": [],
+            "stateMutability": "nonpayable",
+            "type": "function",
+        },
+        {
+            "inputs": [
+                {"internalType": "string", "name": "", "type": "string"},
+                {"internalType": "uint256", "name": "", "type": "uint256"},
+            ],
+            "name": "votes",
+            "outputs": [{"internalType": "string", "name": "", "type": "string"}],
+            "stateMutability": "view",
+            "type": "function",
+        },
+    ]
+
+    def __init__(self):
+        self._contract = None
+
+    @property
+    def contract(self):
+        if not _ethereum_available:
+            raise ValueError("No connected wallet found")
+
+        if not self._contract:
+            provider = _ethers.providers.Web3Provider(ethereum)
+            signer = provider.getSigner()
+            self._contract = _ethers.Contract(self.address, self.abi, signer)
+
+        return self._contract
+
+    def register_ballot(self, ballot):
+        self.contract.createBallot(**ballot.__dict__)
+
+    def cast_vote(self, ballot, ciphertext):
+        self.contract.vote(ballot.uuid, ciphertext)
