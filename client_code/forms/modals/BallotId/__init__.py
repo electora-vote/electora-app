3f718c193a05eeac9c9fcbec0ceb3c9481545123
owen.campbell@empiria.co.uk <owen.campbell@empiria.co.uk>
2023-06-05 15:33:05 +0000

    Remove redundant modal form

--- client_code/forms/modals/BallotId/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._anvil_designer import BallotIdTemplate
-
-
-class BallotId(BallotIdTemplate):
-    def __init__(self, **properties):
-        self.item = ""
-        self.init_components(**properties)
