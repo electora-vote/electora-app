ad379061e0ad1ce060be6d47664903f02ee5e060
Owen Campbell <owen.campbell@empiria.co.uk>
2023-06-07 13:40:01 +0100

    Use rh pane for ballot creation

--- client_code/forms/routes/ballot/Index/__init__.py
+++ client_code/forms/routes/ballot/Index/__init__.py
@@ -5,6 +5,7 @@ import anvil.js
 import anvil
 
 from ..Read import Read
+from ..Create import Create
 from ._anvil_designer import IndexTemplate
 from .Template import Template
 
@@ -16,6 +17,7 @@ columns = [
 
 @routing.route("")
 @routing.route("", url_keys=["ballot_id"])
+@routing.route("", url_keys=["create_ballot"])
 class Index(IndexTemplate):
     def __init__(self, **properties):
         self.ballots = []
@@ -25,8 +27,11 @@ class Index(IndexTemplate):
     def show_ballot(self, uuid):
         ballot = session.LOCAL_STORE.get(Ballot, uuid)
         form = Read(ballot=ballot)
-        main = anvil.get_open_form()
-        main.show_detail(form)
+        anvil.get_open_form().show_detail(form)
+
+    def create_ballot(self):
+        form = Create()
+        anvil.get_open_form().show_detail(form)
 
     def init_tabulator(self):
         self.tabulator.options = session.tabulator_options
@@ -42,10 +47,11 @@ class Index(IndexTemplate):
         routing.set_url_hash(f"?ballot_id={model.uuid}")
 
     def create_button_click(self, **event_args):
-        anvil.js.window.hideDetailPanel()
-        routing.set_url_hash("ballot/create", load_from_cache=False)
+        self.create_ballot()
 
     def form_show(self, **event_args):
         self.refresh_tabulator()
         if "ballot_id" in self.url_dict:
             self.show_ballot(self.url_dict["ballot_id"])
+        if "create_ballot" in self.url_dict:
+            self.create_ballot()
