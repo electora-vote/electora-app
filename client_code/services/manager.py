4b5ecefb6f9ef115354bc1910355875efe070346
James Campbell <james.campbell@tanti.org.uk>
2023-10-07 15:25:54 +0200

    feat: Add event for ballot creation and candidate addition

--- client_code/services/manager.py
+++ client_code/services/manager.py
@@ -1,4 +1,4 @@
-address = "0x53aa5b2DDCe249dF629cd2ffade97dfB166fb05c"
+address = "0x5DFF5bb3b8f210b91EfCa83b13fFCF1db4A2fe06"
 url = f"https://sepolia-blockscout.scroll.io/address/{address}"
 abi = [
     {
@@ -29,7 +29,6 @@ abi = [
         "stateMutability": "nonpayable",
         "type": "function",
     },
-    {"inputs": [], "stateMutability": "nonpayable", "type": "constructor"},
     {
         "inputs": [
             {"internalType": "string", "name": "", "type": "string"},
--- contracts/BallotManager.sol
+++ contracts/BallotManager.sol
@@ -22,6 +22,10 @@ contract BallotManager {
     mapping(string => uint256) public uuidToIndex;
     mapping(string => Candidate[]) public ballotIdToCandidates;
 
+    event ballotCreated(string indexed _ballotId, string name, uint endTime, string sismoGroupId, string candidateGroupId, uint8 dkgRitualId, uint8 protocolVersion);
+    event candidateAdded(string indexed _ballotId, string title, string description, string proof, string invoice);
+
+
     constructor() {
     }
     
@@ -36,6 +40,7 @@ contract BallotManager {
     ) public {
         ballots.push(BallotInfo(_ballotId, _name, _endTime, _sismoGroupId, _candidateGroupId, _dkgRitualId, _protocolVersion));
         uuidToIndex[_ballotId] = ballots.length - 1;
+        emit ballotCreated(_ballotId, _name, _endTime, _sismoGroupId, _candidateGroupId, _dkgRitualId, _protocolVersion);
     }
 
     function getAllBallots() public view returns (BallotInfo[] memory) {
@@ -68,6 +73,7 @@ contract BallotManager {
 
     function addCandidate(string memory _ballotId, string memory _title, string memory _description, string memory _proof, string memory _invoice) external {
         ballotIdToCandidates[_ballotId].push(Candidate(_title, _description, _proof, _invoice));
+        emit candidateAdded(_ballotId, _title, _description, _proof, _invoice);
     }
 
     function getProtocolVersion(string memory _ballotId) external view returns(uint8) {
